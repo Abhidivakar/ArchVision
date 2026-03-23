2a40d203dc4b65bcb79e99c224c5de9b73b7ac65
Abhishek <abhishekdivakar67@gmail.com>
2026-03-24 00:29:53 +0530

    cloud run deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   // We removed rewrites to use a custom Route Handler in src/app/api/[...path]/route.ts
   // for better control over timeouts and error handling.
 };
